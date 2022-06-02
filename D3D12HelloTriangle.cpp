1a8a10c5ef044b33e9089dd4f6b232b642972618
Philipp Jonas <philipp.jonas25@gmail.com>
2022-06-02 19:52:15 +0200

    Changed path to shaders.hsls in D3D12HelloTriangle.cpp

--- D3D12HelloTriangle.cpp
+++ D3D12HelloTriangle.cpp
@@ -218,8 +218,10 @@ void D3D12HelloTriangle::LoadAssets()
 		UINT compileFlags = 0;
 #endif
 
-		ThrowIfFailed(D3DCompileFromFile(GetAssetFullPath(L"shaders.hlsl").c_str(), nullptr, nullptr, "VSMain", "vs_5_0", compileFlags, 0, &vertexShader, nullptr));
-		ThrowIfFailed(D3DCompileFromFile(GetAssetFullPath(L"shaders.hlsl").c_str(), nullptr, nullptr, "PSMain", "ps_5_0", compileFlags, 0, &pixelShader, nullptr));
+		LPCWSTR a = L"shaders.hlsl";
+
+		ThrowIfFailed(D3DCompileFromFile(a, nullptr, nullptr, "VSMain", "vs_5_0", compileFlags, 0, &vertexShader, nullptr));
+		ThrowIfFailed(D3DCompileFromFile(a, nullptr, nullptr, "PSMain", "ps_5_0", compileFlags, 0, &pixelShader, nullptr));
 
 		// Define the vertex input layout.
 		D3D12_INPUT_ELEMENT_DESC inputElementDescs[] =
